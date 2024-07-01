d2b0329a0547917369763431d6a44cadc2b958c7
DESKTOP-AN805P4\user <test@test.com>
2024-07-01 18:56:35 +0900

    2024-07-01 수업

--- src/main/java/com/quiz/booking/bo/BookingBO.java
+++ src/main/java/com/quiz/booking/bo/BookingBO.java
@@ -1,9 +1,11 @@
 package com.quiz.booking.bo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quiz.booking.domain.Booking;
 import com.quiz.booking.mapper.BookingMapper;
@@ -14,7 +16,24 @@ public class BookingBO {
 	@Autowired
 	private BookingMapper bookingMapper;
 	
+	// input: X
+	// output: List<Booking>
 	public List<Booking> getBookingList() {
 		return bookingMapper.selectBookingList();
+	}
+	
+	// input: id
+	// output: int
+	public int deleteBookingById(int id) {
+		return bookingMapper.deleteBookingById(id);
+	}
+
+	public void addBooking(String name, LocalDate date, 
+			int day, int headcount, String phoneNumber) {
+		bookingMapper.insertBooking(name, date, day, headcount, phoneNumber);
+	}
+	
+	public List<Booking> checkBooking(String name, String phoneNumber) {
+		return bookingMapper.selectBooking(name, phoneNumber);
 	}
 }
