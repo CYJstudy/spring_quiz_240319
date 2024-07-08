75f9592bf13264fac34d53d57c8069e77601c162
DESKTOP-AN805P4\user <test@test.com>
2024-07-08 18:52:31 +0900

    2024-07-08 수업

--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -3,11 +3,40 @@ package com.quiz.lesson07.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.quiz.lesson07.entity.RecruitEntity;
 
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer> {
 
-	public List<RecruitEntity> findById(int id);
-	public List<RecruitEntity> findByCompanyId(int id);
+	// JPQL => Entity에 조회
+	// quiz02_2
+	public List<RecruitEntity> findByCompanyId(int companyId);
+	
+	// quiz02_3
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+	
+	// quiz02_4
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+	
+	// quiz02_5
+	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+	
+	// quiz02_6
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int startSalary, int endSalary);
+	
+	// quiz02_7 - native query (DB에 직접 SQL)
+	@Query(value = "select * from `recruit` "
+			+ "where `deadline` > :deadline "
+			+ "and `salary` >= :salary "
+			+ "and `type` = :type "
+			+ "order by `salary` desc"
+			, nativeQuery = true)
+	public List<RecruitEntity> findByDeadLineAndSalaryAndType(
+			@Param("deadline") String deadline, 
+			@Param("salary") int salary, 
+			@Param("type") String type);
+	
+	
 }
